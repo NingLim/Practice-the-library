01ed598694803d3058bad2d3111a6d86ce33d399
NingLim <291710635@qq.com>
2019-06-30 11:12:31 +0800

    vue-router practice

---
+++ router/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <p>导航 ：
+      <router-link to="/">首页</router-link>
+      <router-link to="/hi">Hi页面</router-link>
+      <router-link to="/hi/hi1">-Hi1页面-</router-link>
+      <router-link to="/hi/hi2">-Hi2页面-</router-link>
+    </p>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ router/src/components/Hi.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <router-view class="aaa"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hi',
+  data () {
+    return {
+      msg: 'Hi, I am Ning'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
